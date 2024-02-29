836b775e9fcfb8d9a10a0a2f6e01fae70db868e7
Walton <zjxcdut@gmail.com>
2024-02-28 22:00:27 -0400

    Added Models Pokemon

--- app/src/main/java/com/example/pokemonapp/models/Pokemon.kt
+++ app/src/main/java/com/example/pokemonapp/models/Pokemon.kt
@@ -1,2 +1,17 @@
 package com.example.pokemonapp.models
 
+data class Pokemon (
+    val name: String,
+    val types: Types,
+    val height: Float,
+    val weight: Float
+)
+
+data class Types (
+    val type: Type
+)
+
+data class Type (
+    val name: String,
+    val url: String
+)
