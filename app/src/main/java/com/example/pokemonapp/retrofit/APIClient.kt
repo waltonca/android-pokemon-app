29635b7f459a2668ecad77411a959932bbbf824e
Walton <zjxcdut@gmail.com>
2024-02-28 22:11:03 -0400

    Added retrofit API class

--- app/src/main/java/com/example/pokemonapp/retrofit/APIClient.kt
+++ app/src/main/java/com/example/pokemonapp/retrofit/APIClient.kt
@@ -1,4 +1,19 @@
 package com.example.pokemonapp.retrofit
 
-class APIClient {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object APIClient {
+    private const val BASE_URL = "https://pokeapi.co"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val APIService by lazy {
+        retrofit.create(APIService::class.java)
+    }
 }
--- app/src/main/java/com/example/pokemonapp/retrofit/APIService.kt
+++ app/src/main/java/com/example/pokemonapp/retrofit/APIService.kt
@@ -1,4 +1,13 @@
 package com.example.pokemonapp.retrofit
 
-class APIService {
+import com.example.pokemonapp.models.Pokemon
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface APIService {
+    @GET("api/v2/pokemon/{pokemonName}")
+    suspend fun getPokemon (
+        @Query("pokemonName") pokemonName: String
+    ): Pokemon
+
 }
